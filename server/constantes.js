2fb6ae4aded75dafdd23cd93161f8b92efb8e8d5
emaggi <esrma3010@gmail.com>
2022-09-30 16:47:51 -0400

    fitro y orden ok

---
+++ server/constantes.js
@@ -0,0 +1,10 @@
+export const ORDERS = {
+    "disponibilidad": "order by existencia DESC ",
+    "priceDesc": "order by precio DESC ",
+    "priceAsc": "order by precio ASC ",
+
+  };
+export const ITEM_POR_PAGINA=20;
+export const CATEGORY ={
+  "general": "where categoria = 'general'",
+}
--- server/controllers/productos.controllers.js
+++ server/controllers/productos.controllers.js
@@ -1,11 +1,16 @@
 import { pool } from "../db.sql.js";
+import { ORDERS, ITEM_POR_PAGINA, CATEGORY } from "../constantes.js";
 
 export const getTasks = async (req, res) => {
   try {
-    let itemPorPagina= 20;
-    let query= req.params.p >1 ? `SELECT * FROM productos_view limit ${(req.params.p - 1)*10 },${itemPorPagina}`: `SELECT * FROM productos_view limit ${itemPorPagina}`
+    var orderBy = req.query['orderBy'];
+    var category = req.query['category']? req.query['category']: '' ;
+    console.log("categoria", category);
+    let itemPorPagina= ITEM_POR_PAGINA;
+    let query= req.params.p >1 ? `SELECT * FROM productos_view_api ${ CATEGORY[category]? CATEGORY[category]: 'where 1=2' }  ${ ORDERS[orderBy] } limit ${(req.params.p - 1)*itemPorPagina },${itemPorPagina}`: `SELECT * FROM productos_view_api ${ CATEGORY[category]? CATEGORY[category]: 'where 1=2' }  ${ ORDERS[orderBy] } limit ${itemPorPagina}`
+    console.log("query", query)
     const [result] = await pool.query(query);
-    const [total] = await pool.query("SELECT count(id) as total FROM productos_view ");
+    const [total] = await pool.query("SELECT count(id) as total FROM productos_view_api ");
    
     // result['blob_imagen']= 
     res.json({total: Math.ceil(total[0].total/itemPorPagina), itemPorPagina: itemPorPagina, currentPage: parseInt(req.params.p),data:[ ...result]});
@@ -15,8 +20,11 @@ export const getTasks = async (req, res) => {
 };
 export const getAllProducts = async (req, res) => {
   try {
+    var orderBy = req.query['orderBy'];
+    
+    console.log(req.query)
     const [result] = await pool.query(
-      "SELECT * FROM productos_view "
+      `SELECT * FROM productos_view_api ${ ORDERS[orderBy] }`
     );
     if (result.length == 0) {
       return res.status(404).json({ mensaje: "Producto no encontrado" });
@@ -29,7 +37,7 @@ export const getAllProducts = async (req, res) => {
 export const getTask = async (req, res) => {
   try {
     const [result] = await pool.query(
-      "SELECT * FROM productos_view where cod_articulo = ?",
+      "SELECT * FROM productos_view_api where cod_articulo = ?",
       [req.params.id]
     );
     if (result.length == 0) {
